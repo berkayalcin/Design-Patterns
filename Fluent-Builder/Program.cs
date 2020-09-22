636ed79ee43ef8880ec6551dffb451f1cf444745
berkay.yalcin <berkay.yalcin@trendyol.com>
2020-09-22 21:46:06 +0300

    Add Fluent Builder

---
+++ Fluent-Builder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Builder;
+
+namespace Fluent_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HtmlBuilder("ul");
+            builder
+                .AddChild("li","Hello")
+                .AddChild("li","World");
+            Console.WriteLine(builder);
+        }
+    }
+}
