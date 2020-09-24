ada13ea9ab31a02d07e97d9e53a84fcabfa92184
berkay.yalcin <berkay.yalcin@trendyol.com>
2020-09-24 20:17:35 +0300

    Add Async Factory

---
+++ Creational-Patterns/Async-Factory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Async_Factory
+{
+    public class Foo
+    {
+        private Foo()
+        {
+            //
+        }
+
+        private async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+
+            return this;
+        }
+
+        public static Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return result.InitAsync();
+        }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Foo.CreateAsync();
+        }
+    }
+}
