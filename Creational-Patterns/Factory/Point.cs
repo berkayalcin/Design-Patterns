fb11a1a0b9673515bc8f4727eb7cb63ca381316d
berkay.yalcin <berkay.yalcin@trendyol.com>
2020-09-24 20:13:16 +0300

    Add Factory Method

---
+++ Creational-Patterns/Factory/Point.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Factory
+{
+    public class Point
+    {
+        private double _x, _y;
+
+        /// <summary>
+        /// Factory Method
+        /// </summary>
+        /// <param name="x">X Coordinate</param>
+        /// <param name="y">Y Coordinate</param>
+        /// <returns></returns>
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+
+        private Point(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+    }
+}
---
+++ Creational-Patterns/Factory/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = Point.NewCartesianPoint(1, 3);
+            var polarPoint = Point.NewPolarPoint(1.0, Math.PI / 2);
+        }
+    }
+}
