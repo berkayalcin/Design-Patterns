2357640ab7578ac9f6385c3d6cb3ba6e58dd0bab
berkay.yalcin <berkay.yalcin@trendyol.com>
2020-09-27 00:03:56 +0300

    Add Adapter-Decorator

---
+++ Creational-Patterns/Decorator-Patterns/Adapter-Decorator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Adapter_Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStringBuilder builder = "Builder";
+            builder += " My String Builder";
+            builder.Append("Test ");
+
+            Console.WriteLine(builder);
+        }
+    }
+}
