ee59c7c5d6fec7a9d02f33b9a10e79d104d4ec59
berkay.yalcin <berkay.yalcin@trendyol.com>
2020-09-26 23:57:51 +0300

    Add Custom String Builder Decorator

---
+++ Creational-Patterns/Decorator-Patterns/Custom-String-Builder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Custom_String_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var codeBuilder = new CodeBuilder();
+            codeBuilder.AppendLine("class Foo")
+                .AppendLine("{")
+                .AppendLine("}");
+            Console.WriteLine(codeBuilder);
+        }
+    }
+}
