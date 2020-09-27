670c4f8eba725e9eaf4c21b353f42756e8644170
berkay.yalcin <berkay.yalcin@trendyol.com>
2020-09-27 17:22:37 +0300

    Add Value Proxy

---
+++ Structural-Patterns/Proxy-Patterns/Value-Proxy/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Value_Proxy
+{
+    [DebuggerDisplay("{value*100.0f}%")]
+    public struct Percentage : IEquatable<Percentage>
+    {
+        private readonly float _value;
+
+        internal Percentage(float value)
+        {
+            _value = value;
+        }
+
+        public static float operator *(float f, Percentage p)
+        {
+            return f * p._value;
+        }
+
+        public static float operator +(Percentage f, Percentage p)
+        {
+            return f._value + p._value;
+        }
+
+        public override string ToString()
+        {
+            return $"{_value * 100}%";
+        }
+
+        public bool Equals(Percentage other)
+        {
+            return _value.Equals(other._value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Percentage other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+
+        public static bool operator ==(Percentage left, Percentage right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Percentage left, Percentage right)
+        {
+            return !left.Equals(right);
+        }
+    }
+
+    public static class PercentageExtensions
+    {
+        public static Percentage Percent(this int value)
+        {
+            return new Percentage(value / 100.0f);
+        }
+
+        public static Percentage Percent(this float value)
+        {
+            return new Percentage(value / 100.0f);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(10f * 5.Percent());
+            Console.WriteLine(10.Percent() + 5.Percent());
+        }
+    }
+}
