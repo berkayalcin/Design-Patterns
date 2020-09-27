8e43f4f05e01a138cd62edaeefb1a3f3737e38c9
berkay.yalcin <berkay.yalcin@trendyol.com>
2020-09-27 23:01:58 +0300

    Add Iterator Patterns

---
+++ Behavioral-Patterns/Iterator/Array-Backed-Properties/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Array_Backed_Properties
+{
+    public class Creature : IEnumerable<int>
+    {
+        private int[] _stats = new int[3];
+
+        public int Strength
+        {
+            get => _stats[0];
+            set => _stats[0] = value;
+        }
+
+        public int Agilitiy
+        {
+            get => _stats[1];
+            set => _stats[1] = value;
+        }
+
+        public int Intelligence
+        {
+            get => _stats[2];
+            set => _stats[2] = value;
+        }
+
+        public double AverageStat => _stats.Average();
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return _stats.AsEnumerable().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var creature = new Creature();
+            creature.Agilitiy = 10;
+            creature.Strength = 10;
+            creature.Intelligence = 10;
+
+            foreach (var prop in creature)
+            {
+                Console.WriteLine(prop);
+            }
+        }
+    }
+}
---
+++ Behavioral-Patterns/Iterator/Iterator-Pattern/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Iterator_Pattern
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Left, Right;
+        public Node<T> Parent;
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node(T value, Node<T> left, Node<T> right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+
+            Left.Parent = Right.Parent = this;
+        }
+    }
+
+
+    public class BinaryTree<T>
+    {
+        private Node<T> _root;
+
+        public BinaryTree(Node<T> root)
+        {
+            _root = root;
+        }
+
+        public IEnumerable<Node<T>> InOrder
+        {
+            get
+            {
+                IEnumerable<Node<T>> Traverse(Node<T> current)
+                {
+                    if (current.Left != null)
+                        foreach (var left in Traverse(current.Left))
+                            yield return left;
+
+                    yield return current;
+
+                    if (current.Right != null)
+                        foreach (var right in Traverse(current.Right))
+                            yield return right;
+                }
+
+                foreach (var node in Traverse(_root))
+                {
+                    yield return node;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new Node<int>(
+                1,
+                new Node<int>(2),
+                new Node<int>(3));
+
+            var tree = new BinaryTree<int>(root);
+            Console.Write(string.Join(',',
+                tree.InOrder.Select(node => node.Value)));
+        }
+    }
+}
