b46cb17a8fe545da67d6ca7eb647d3f2dc73a33e
berkay.yalcin <berkay.yalcin@trendyol.com>
2020-09-27 17:45:23 +0300

    Add Composite-Proxy-Property

---
+++ Structural-Patterns/Proxy-Patterns/Composite-Proxy-Property/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Composite_Proxy_Property
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Structural-Patterns/Proxy-Patterns/Composite-Proxy/Program.cs
+++ Structural-Patterns/Proxy-Patterns/Composite-Proxy/Program.cs
@@ -6,13 +6,14 @@ namespace Composite_Proxy
     {
         static void Main(string[] args)
         {
+            // Array of Structures
             var creatures = new Creature[100];
             foreach (Creature creature in creatures)
             {
                 creature.X++;
             }
 
-
+            // Structure of Arrays
             var creatures2 = new Creatures(100);
             foreach (Creatures.CreatureProxy creature in creatures2)
             {
